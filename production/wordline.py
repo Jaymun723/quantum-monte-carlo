ebaff164ad160e03b366daf85f1d68d79ff81948
Victor CARO <victor.caro@polytechnique.edu>
2025-11-04 16:57:23 +0100

    Added problem passing to wordline

--- production/wordline.py
+++ production/wordline.py
@@ -1,6 +1,8 @@
+from problem import Problem
+
 class Wordline: # "w"
-    def __init__(self):
-        pass
+    def __init__(self, problem: Problem):
+        self.problem = problem
 
     def weight(self): # Omega(w)
         pass
