88122eb6e2c6e2797e1a2e20b75a35ada8007b02
Victor CARO <victor.caro@polytechnique.edu>
2025-12-01 16:55:05 +0100

    better weights

--- production/wordline.py
+++ production/wordline.py
@@ -13,42 +13,49 @@ class Wordline: # "w"
     """
     def __init__(self, problem: Problem, spins = None):
         self.problem = problem
+        self.spins = self._initialize_state() if spins is None else spins
+        self._weight = None
 
-    def weight(self): # Omega(w)
-        # `P` is the matrix that changes basis from:
-        # the *canonical basis*: |up,up>, |up, down>, |down, up>, |down, down>
-        # to the *two site basis*: |up,up>, sqrt(0.5) (|up, down> + |down, up>), sqrt(0.5) (|up, down> - |down, up>), |down, down>
-        # Notice : we have P^T = P^-1 = P
-        P = np.array([
-            [1, 0, 0, 0],
-            [0, 1/np.sqrt(2), 1/np.sqrt(2), 0],
-            [0, 1/np.sqrt(2), -1/np.sqrt(2), 0],
-            [0, 0, 0, 1],
-        ])
-        # The two site hamiltonian in its basis
-        H_two_site = np.array([
-            [self.problem.J_z / 4, 0, 0, 0],
-            [0, (-self.problem.J_z / 4 + self.problem.J_x / 2), 0, 0],
-            [0, 0, (-self.problem.J_z / 4 - self.problem.J_x / 2), 0],
-            [0, 0, 0, self.problem.J_z / 4]
-        ])
-        exp_H_two_site = P @ np.diag(np.exp(np.diag(H_two_site))) @ P
+    @property
+    def weight(self):
+        if self._weight == None:
+            self._weight = self._compute_weight()
+        return self._weight
 
-        weight = 1
+    def _compute_weight(self): # Omega(w)
+        """
+        Compute the weight $\Omega(w)$ of the wordline configuration.
+        """
+        n = self.problem.n_sites
+        m = self.problem.m
+        weight = 1.0
 
-        # <\sigma_{\tau+1}| exp(-\delta\tau H_{1|2}) |\sigma_\tau>
-        for tau in range(0, 2 * self.problem.m):
-            tau_plus_one = tau + 1 % (2 * self.problem.m)
-
-            # <\sigma_{2i, \tau+1},\sigma{2i+1, \tau+1}| exp(-\delta\tau H_two_site) |\sigma_{2i, \tau},\sigma{2i+1, \tau}>
-            for i in range(0, self.problem.n_sites // 2):
-                j = 2*i
+        for tau in range(2*m):
+            for i in range(n//2):
+                spins_selected = (2*i, (2*i+1) % n)
                 if tau % 2 == 1:
-                    j = 2*i + 1
+                    spins_selected = ((2*i+1)%n, (2*i+2) % n)
+                
+                bra = list(self.spins[(tau + 1) % (2*m), spins_selected])
+                ket = list(self.spins[tau, spins_selected])
+                # print(f"<{(tau+1) % (2*m)}, {spins_selected}|={bra}")
+                # print(f"|{tau}, {spins_selected}>={ket}")
 
-                bra = self.grid[tau_plus_one, j:j+2]  # <\sigma_{j, \tau+1},\sigma{j+1, \tau+1}|
-                ket = self.grid[tau, j:j+2] # |\sigma_{j, \tau},\sigma{j+1, \tau}>
-                weight *= bra.T @ exp_H_two_site @ ket
+                if (ket == [1, -1] and bra == [1, -1]) or (ket == [-1, 1] and bra == [-1, 1]):
+                    # print("a")
+                    weight *= np.exp(self.problem.delta_tau * self.problem.J_z / 4) * np.cosh(self.problem.delta_tau * self.problem.J_x / 2)
+                elif (ket == [1, -1] and bra == [-1, 1]) or (ket == [-1, 1] and bra == [1, -1]):
+                    # print("b")
+                    weight *= -np.exp(self.problem.delta_tau * self.problem.J_z / 4) * np.sinh(self.problem.delta_tau * self.problem.J_x / 2)
+                elif (ket == [1, 1] and bra == [1, 1]) or (ket == [-1, -1] and bra == [-1, -1]):
+                    # print("c")
+                    weight *= np.exp(-self.problem.delta_tau * self.problem.J_z / 4)
+                else:
+                    weight = 0.0
+                    break
+
+            if weight == 0.0:
+                break
 
         return weight
 
