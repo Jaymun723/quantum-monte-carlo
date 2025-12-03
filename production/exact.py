9fa3b54ef077f21e1a25f5da2167a87edccf6322
Victor CARO <victor.caro@polytechnique.edu>
2025-12-03 17:31:19 +0100

    Monte carlo plz

--- production/exact.py
+++ production/exact.py
@@ -5,6 +5,7 @@ import itertools
 from tqdm import tqdm
 import functools
 
+
 class ExactSolver:
     """
     Solves the problem using the $2^n$ matrices.
@@ -12,25 +13,35 @@ class ExactSolver:
     Arguments:
     - problem (Problem): The settings for the solver.
     """
+
     def __init__(self, problem: Problem):
         self.problem = problem
 
         # Base matricies $S^x, S^y, S^z$
         self.S = {
-            "x": 0.5 * np.array([[0,1],[1, 0]], dtype="complex128"),
-            "y": 0.5 * np.array([[0,-1j], [1j,0]], dtype="complex128"),
-            "z": 0.5 * np.array([[1, 0], [0, -1]], dtype="complex128")
+            "x": 0.5 * np.array([[0, 1], [1, 0]], dtype="complex128"),
+            "y": 0.5 * np.array([[0, -1j], [1j, 0]], dtype="complex128"),
+            "z": 0.5 * np.array([[1, 0], [0, -1]], dtype="complex128"),
         }
 
         self.H = self.compute_H()
         self.exp_H = expm(-self.problem.beta * self.H)
 
-    def compute_S(self, i, dir):
+    def compute_S(self, i, ax):
         """Returns $S^dir_i S^dir_{i+1}$."""
-        S = self.S[dir] if i == 0 else np.eye(2)
-        for j in range(1, self.problem.n_sites):
-            this_S = self.S[dir] if i == j or i == j+1 else np.eye(2)
-            S = np.kron(S, this_S)
+        # S = self.S[dir] if i == 0 else np.eye(2)
+        # for j in range(1, self.problem.n_sites):
+        #     this_S = self.S[dir] if i == j or i == j+1 else np.eye(2)
+        #     S = np.kron(S, this_S)
+        n = self.problem.n_sites
+        ops = [np.eye(2, dtype="complex128") for _ in range(n)]
+        ops[i] = self.S[ax]
+        ops[(i + 1) % n] = self.S[ax]
+
+        S = ops[0]
+        for op in ops[1:]:
+            S = np.kron(S, op)
+
         return S
 
     def compute_H(self):
@@ -41,18 +52,18 @@ class ExactSolver:
             H += self.problem.J_x * self.compute_S(i, "y")
             H += self.problem.J_z * self.compute_S(i, "z")
         return H
-    
+
     def compute(self, obs):
         """
         Computes the expected value of the observable provided.
         $<O>=tr(e^{-\\beta H}O)/tr(e^{\\beta H})$
         """
-        return np.trace(self.exp_H * obs) / np.trace(self.exp_H)
-    
+        return np.trace(self.exp_H @ obs) / np.trace(self.exp_H)
+
     @functools.cached_property
     def energy(self):
         return self.compute(self.H)
-    
+
     def all_configs(self):
         # initial_config = np.zeros((2*self.problem.m, self.problem.n_sites))
 
@@ -70,8 +81,11 @@ class ExactSolver:
         total_configs = 2 * self.problem.m * self.problem.n_sites
         flats_configs = itertools.product([1, -1], repeat=total_configs)
 
-        for flat_config in tqdm(flats_configs, total=total_configs, desc="Generating configs"):
-            config = np.array(flat_config).reshape((2*self.problem.m), self.problem.n_sites)
-            
-            yield config
+        for flat_config in tqdm(
+            flats_configs, total=total_configs, desc="Generating configs"
+        ):
+            config = np.array(flat_config).reshape(
+                (2 * self.problem.m), self.problem.n_sites
+            )
 
+            yield config
