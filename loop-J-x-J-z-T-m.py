ebc8e8f8d55db938ee40eb6d26db76411b8e3090
Victor Caro <victor.caro@polytechnique.edu>
2025-12-09 20:33:43 +0100

    Test with  -J_z

---
+++ loop-J-x-J-z-T-m.py
@@ -0,0 +1,21 @@
+"""
+# Computations of Energy, Spins with perform_vertex_loop_update, fixed T
+
+Short name: `loop-J-x-J-z-T-m` (names of fixed parameters)
+"""
+
+from production import MonteCarlo, Problem, loop_update
+from pathlib import Path
+
+ns = [4, 6, 8, 10, 12, 14]
+problems = [Problem(n_sites=n, J_x=2.2, J_z=-1.0, temperature=1.0, m=6) for n in ns]
+
+mc = MonteCarlo(
+    problems,
+    loop_update,
+    n_cycles=5_000,
+    n_rep=10,
+    save_folder=Path("./data/loop-J-x-J-z-T-m"),
+)
+
+mc.run()
---
+++ loop-n-J-x-J-z-m.py
@@ -0,0 +1,24 @@
+"""
+# Computations of Energy, Spins with loop_updates, fixed m
+
+Short name: `loop-n-J-x-J-z-m` (names of fixed parameters)
+"""
+
+from production import MonteCarlo, Problem, loop_update
+from pathlib import Path
+import numpy as np
+
+temperatures = np.concat([np.linspace(0.1, 10, 7), np.linspace(20, 100, 3)])
+problems = [
+    Problem(n_sites=8, J_x=2.2, J_z=-1.0, temperature=t, m=6) for t in temperatures
+]
+
+mc = MonteCarlo(
+    problems,
+    loop_update,
+    n_cycles=5_000,
+    n_rep=10,
+    save_folder=Path("./data/loop-n-J-x-J-z-m"),
+)
+
+mc.run()
